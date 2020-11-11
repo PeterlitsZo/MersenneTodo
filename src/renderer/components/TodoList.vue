5a34417f9ae2cfb4069b49f7cef0c5feedcb6883
Peterlits Zo <peterlitszo@outlook.com>
2020-11-11 12:13:09 +0800

    feat: (TodoList) init TodoList and change router
    Init the components TodoList and then change router to pointer it.

---
+++ src/renderer/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="noselect">
+    <h1>TodoList</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TodoList'
+  }
+</script>
+
+<style>
+  .noselect {
+    user-select: none;
+  }
+</style>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -7,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      name: 'TodoList',
+      component: require('@/components/TodoList').default
     },
     {
       path: '*',
