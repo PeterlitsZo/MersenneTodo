d06ffa0282b04e441f7f91e53749677a72ad6e2e
Peterlits Zo <peterlitszo@outlook.com>
2020-11-11 14:36:51 +0800

    feat: (TodoList/Bar) Add Bar component
    The Bar is the subcomponent for TodoList.

--- src/renderer/components/TodoList.vue
+++ src/renderer/components/TodoList.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="noselect">
     <h1>TodoList</h1>
+    <div v-for="bar in bars">
+      <Bar :obj="bar" />
+    </div>
   </div>
 </template>
 
 <script>
+  import Bar from './TodoList/Bar.vue'
+
   export default {
-    name: 'TodoList'
+    name: 'TodoList',
+    data: () => {
+      return {
+        bars: [{title: 'temp1'}, {title: 'temp2'}, {title: 'temp3'}]
+      }
+    },
+    components: {
+      'Bar': Bar
+    }
   }
 </script>
 
---
+++ src/renderer/components/TodoList/Bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ obj.title }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Bar',
+    props: {
+      obj: Object
+    }
+  }
+</script>
+
+<style>
+</style>
