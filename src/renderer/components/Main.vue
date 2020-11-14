dbc92f0e5b40e204f2d012939e7e80a253f1a7ef
Peterlits Zo <peterlitszo@outlook.com>
2020-11-14 14:02:29 +0800

    fix : (Main) make the Date string -> Date obj
    Because after the JSON.stringify, the Date object will be a Date string,
    so after JSON.parse it, it will be a `string` but not `object`, I make
    it pass a function after JSON.parse.

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -10,7 +10,11 @@
   function normalBars (bars) {
     var result = bars.slice()
     for (var i = 0; i < result.length; i++) {
-      if (!result[i]['time']) result[i]['time'] = new Date()
+      if (!result[i]['time']) {
+        result[i]['time'] = new Date()
+      } else {
+        result[i]['time'] = new Date(result[i]['time'])
+      }
       if (!result[i]['OK']) result[i]['OK'] = false
       if (!result[i]['title']) /* raise a error */;
     }
@@ -27,6 +31,11 @@
     methods: {
       update (bars) {
         this.bars = bars
+        fs.writeFile('.mersennetodo.json', JSON.stringify(this.bars), (err) => {
+          if (err) {
+            console.error('Cannot write to .mersennetodo.json!')
+          }
+        })
       }
     },
     created () {
@@ -36,13 +45,18 @@
           this.update(normalBars([
             {title: '1. Click on my text to set my state (OK/Todo)'},
             {title: '2. Want to set a new Todo? Edit at below editor, then press `Ctrl+Enter`'},
-            {title: ['3. A Useless Todo bar? move on me and you can find a bin '
+            {title: ['3. A Useless Todo bar? move on me and you can find a bin ',
               'icon, then click on the my delete bin icon'].join('')}
           ]))
           console.log('err, do not find .mersennetodo.json, set it to: ', this.bars)
+          fs.writeFile('.mersennetodo.json', JSON.stringify(this.bars), (err) => {
+            if (err) {
+              console.error('Cannot write to .mersennetodo.json!')
+            }
+          })
           return
         }
-        console.log('data: ', data)
+        this.bars = normalBars(JSON.parse(data))
       })
     },
     components: {
