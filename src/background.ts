b97ba73f72d6ef9d403b0fefa1cdff8c7495a65c
Peterlits Zo <peterlitszo@outlook.com>
2020-11-26 19:32:50 +0800

    feat: (*) enTS main.ts and add ts plugin

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+// import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -18,7 +18,8 @@ async function createWindow() {
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+      // This application do not need remote content. leave it as true
+      nodeIntegration: true
     }
   })
 
